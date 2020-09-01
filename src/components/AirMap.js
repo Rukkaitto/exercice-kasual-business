1a7218c451367d9142b7b28f4d0299e23e1100f2
Lucas Goudin <goudinlucas@gmail.com>
2020-09-01 17:19:51 +0200

    Added MarkerCluster

--- src/components/AirMap.js
+++ src/components/AirMap.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import MarkerClusterGroup from "react-leaflet-markercluster";
 
 export class AirMap extends React.Component {
    constructor() {
@@ -16,21 +17,23 @@ export class AirMap extends React.Component {
                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {this.props.measurements.map((measurement, key) => { // Maps the measurements to corresponding markers and popups
-               if (!measurement["coordinates"]["longitude"] || !measurement["coordinates"]["latitude"]) { // Checks if the latitude or longitude are null or undefined
-                  return null;
-               }
-               const markerPosition = [measurement["coordinates"]["latitude"], measurement["coordinates"]["longitude"]];
-               return (
-                  <Marker key={key} position={markerPosition}>
-                     <Popup key={key}>
-                        <b>Location</b> : {measurement["city"]} <br />
-                        <b>Date</b> : {measurement["date"]["local"]} <br />
-                        <b>Value</b> : {measurement["value"]} {measurement["unit"]}
-                     </Popup>
-                  </Marker>
-               );
-            })}
+            <MarkerClusterGroup>
+               {this.props.measurements.map((measurement, key) => { // Maps the measurements to corresponding markers and popups
+                  if (!measurement["coordinates"]["longitude"] || !measurement["coordinates"]["latitude"]) { // Checks if the latitude or longitude are null or undefined
+                     return null;
+                  }
+                  const markerPosition = [measurement["coordinates"]["latitude"], measurement["coordinates"]["longitude"]];
+                  return (
+                     <Marker key={key} position={markerPosition}>
+                        <Popup key={key}>
+                           <b>Location</b> : {measurement["city"]} <br />
+                           <b>Date</b> : {measurement["date"]["local"]} <br />
+                           <b>Value</b> : {measurement["value"]} {measurement["unit"]}
+                        </Popup>
+                     </Marker>
+                  );
+               })}
+            </MarkerClusterGroup>
          </Map>
       )
    }
